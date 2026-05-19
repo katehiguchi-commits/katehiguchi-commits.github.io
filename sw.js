58559397e734a3df7394deea3a00f39b6fbdd09a
katehiguchi-commits <kate.higuchi@gmail.com>
2026-05-19 11:10:34 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE = 'shukatsu-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'shukatsu-v2';
+const FILES = [
+  '/shukatsu/',
+  '/shukatsu/index.html',
+  '/shukatsu/manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -15,6 +19,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/shukatsu/index.html')))
   );
 });
